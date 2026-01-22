a72b42a70e88e8a58f5191a8d3bfce1ff354ee5b
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-01-22 19:26:22 +0100

    made changes

--- exercises.js
+++ exercises.js
@@ -37,7 +37,7 @@ console.log("The return value of number is", + result);
 
 //5. Profile Function-
 const profile = (firstName, age, isLikeCoding) => {
-    console.log("Hi, I am " + firstName + "." + "I am " + age + " " + "years old, and it is" + " " + isLikeCoding + " that i like coding.");
+    console.log(`Hi, I am  ${firstName}. I am ${age} years old, and it is ${isLikeCoding }, that i like coding.`);
     // firstName = "Sonal";
     // age = 38;
     // isLikeCoding = true
@@ -55,38 +55,38 @@ greetWithTime("Sonal", "evening");
 
 
 //7. Calculator Function-
-const calculator= (a, b , operator) =>{
- if(operator === "add"){
-    return a + b;
-} else if (operator === "subtract") {
-    return a - b ;
-} else if ( operator === "multiply"){
-    return a*b ;
-} else if (operator === "divide"){
-    return a/b;
-} else {
-    return "invalid operator";
+const calculator = (a, b, operator) => {
+    if (operator === "add") {
+        return a + b;
+    } else if (operator === "subtract") {
+        return a - b;
+    } else if (operator === "multiply") {
+        return a * b;
+    } else if (operator === "divide") {
+        return a / b;
+    } else {
+        return "invalid operator";
+    }
 }
-}
-console.log(calculator(10,6,"add"));
-console.log(calculator(10,6,"subtract"));
-console.log(calculator(10,6,"multiply"));
-console.log(calculator(18,6,"divide"));
-console.log(calculator(10,6,""));
+console.log(calculator(10, 6, "add"));
+console.log(calculator(10, 6, "subtract"));
+console.log(calculator(10, 6, "multiply"));
+console.log(calculator(18, 6, "divide"));
+console.log(calculator(10, 6, ""));
 
 
 //8. Arrow function 
-const arrowSquare= (number) => {
-    return number*number;
+const arrowSquare = (number) => {
+    return number * number;
 }
 console.log(arrowSquare(11));
 
 
 //9. Bonus Challenge-
-const isAdult= (age) =>{
-    if(age >= 18) {
-return "Hello you are adult!"
-    } else if(age <= 18){
+const isAdult = (age) => {
+    if (age >= 18) {
+        return "Hello you are adult!"
+    } else if (age <= 18) {
         return "Hello, you are not adult, you need to wait!"
     }
 };
